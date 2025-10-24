17353d9191fde497f746ed4053033c0c7d41debf
Héctor Villar Palacios <rallohector@gmail.com>
2025-10-24 16:20:07 +0000

    donde está el icono del escudo que estabas utilizando antes?

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 
-export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
+export const Logo = ({ className }: { className?: string }) => (
   <svg
+    className={className}
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     fill="none"
@@ -9,10 +10,9 @@ export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    {...props}
   >
-    <path d="M8 9h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-    <path d="M12 9h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2" />
-    <path d="M6 13h4" />
+    <path d="M12 2L2 7l10 5 10-5-10-5z" />
+    <path d="M2 17l10 5 10-5" />
+    <path d="M2 12l10 5 10-5" />
   </svg>
 );
